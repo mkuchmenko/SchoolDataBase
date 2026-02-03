084818644760d87bbe303dcf2c55e85d9554268c
m.kuchmenko <kuchmenko.maria.10@gmail.com>
2026-02-03 14:34:36 +0200

    Completed the second block of Praktychna-N1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from csv import DictWriter
 
 with open("config.txt", "r") as file:
@@ -11,16 +12,26 @@ with open('students.csv', "r", encoding='utf-8') as file:
         i['score']=int(i['score'])
 
 retest_list=[]
+success_list=[]
 for st in data:
     if st['score']<85:
         retest_list.append(st)
+    else:
+        success_list.append(st)
 
 readnames=['id','name','surname','score']
 
 with open("retest.csv", "w", newline="", encoding="utf-8") as csvfile:
-    write=DictWriter(csvfile, fieldnames=readnames, delimiter=',')
+    write=csv.DictWriter(csvfile, fieldnames=readnames, delimiter=',')
     write.writeheader()
     write.writerows(retest_list)
+
+for st in  success_list:
+    st['passed']='True'
+
+with open('best_students.json', 'w', encoding='utf-8') as jsonfile:
+    json.dump(success_list, jsonfile, ensure_ascii=False, indent=2)
+
 
 
 
